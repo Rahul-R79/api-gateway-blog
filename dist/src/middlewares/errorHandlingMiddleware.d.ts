dfd89579681b7477a00e53ed7a72117002fb0ef4
Rahul-R79 <rahul.devworks@gmail.com>
2025-12-17 18:18:02 +0530

    Remove dist folder from repository and update .gitignore

--- dist/src/middlewares/errorHandlingMiddleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export declare function errorHandler(err: unknown, req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=errorHandlingMiddleware.d.ts.map
